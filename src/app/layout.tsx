5be24568fc6fd30cdf1834c66a5bafde37085dcf
dryvius <duanpeihua@hotmail.com>
2025-05-28 11:54:28 +0800

    fix:build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import Header from "@/components/header";
-import { ADLaM_Display } from "next/font/google";
+// import { ADLaM_Display } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 
 // google 字体
-const adlam = ADLaM_Display({
-    subsets: ["latin"],
-    weight: "400",
-});
+// const adlam = ADLaM_Display({
+//     subsets: ["latin"],
+//     weight: "400",
+// });
 // 自定义字体
 const myFont = localFont({
     src: "./fonts/UISDC-BiaoTiHei.ttf",
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,8 +6,7 @@ export default function NotFound() {
             <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
                 <div className="max-w-md">
                     <div className="text-5xl font-dark font-bold">404</div>
-                    <p className="text-2xl md:text-3xl font-light leading-normal">Sorry we couldn't find this page. </p>
-                    <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+                    <p className="mb-8">But don&apos;t worry, you can find plenty of other things on our homepage.</p>
 
                     <Link
                         href="/"
